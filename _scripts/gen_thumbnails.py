ce28834a8918eff77adf585b2c7b21f66f80509f
Charles Smith <techdocsmith@gmail.com>
2026-08-03 19:42:59 -0700

    add script to generate thumbnails, first batch of thumbnails

---
+++ _scripts/gen_thumbnails.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import os
+
+# set image locations
+image_dir = "/Users/charles/codebase/charles-o-smith.github.io/images/"
+thumb_dir = "/Users/charles/codebase/charles-o-smith.github.io/thumbs/"
+
+def gen_all(image_dir, thumb_dir):
+    for file in os.listdir(image_dir):
+        thumb = (thumb_dir+"thumb_" + file)
+        try:
+            image = Image.open(image_dir+file)
+            h = image.size[0]
+            w = image.size[1]
+            if h > 1000:
+                thumb_h = h//10
+                thumb_w = w//10
+                image.thumbnail((thumb_h, thumb_w))
+                image.save(thumb, "JPEG")
+            else:
+                image.thumbnail((h, w))
+                image.save(thumb, "JPEG")
+        except:
+            print(f'Not processing {file}')
+
+gen_all(image_dir, thumb_dir)
